63a14aa7b90e24947d1d72299c3d27d043203048
CptnCat <78860645+CptnCat@users.noreply.github.com>
2026-04-12 17:04:34 +0200

    s

--- windgate.lua
+++ windgate.lua
@@ -196,4 +196,4 @@ local Slider = WorldTab:Slider({
     end
 })
 print("[WINDGATE DEBUG] Slider erstellt ✓")
-print("[WINDGATE DEBUG] Script vollständig geladen 
+print("[WINDGATE DEBUG] Script vollständig geladen")
