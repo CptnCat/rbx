547549ebdefd6eeca04271f3ae308d48518a215f
CptnCat <78860645+CptnCat@users.noreply.github.com>
2026-04-30 22:19:48 +0200

    sd

--- windgate.lua
+++ windgate.lua
@@ -8,14 +8,14 @@ local TweenService = game:GetService("TweenService")
 -- AUTO-RERUN AFTER TELEPORT --
 local queueteleport = queue_on_teleport or (syn and syn.queue_on_teleport) or (fluxus and fluxus.queue_on_teleport)
 
-if queueteleport then
-    queueteleport(game:HttpGet("https://raw.githubusercontent.com/CptnCat/rbx/main/windgate.lua"))
-    print("[WINDGATE] queue_on_teleport registriert.")
-else
-    warn("[WINDGATE] queue_on_teleport nicht verfügbar in diesem Executor.")
-end
+local TeleportCheck = false
+Players.LocalPlayer.OnTeleport:Connect(function(State)
+	if KeepInfYield and (not TeleportCheck) and queueteleport then
+		TeleportCheck = true
+		queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/CptnCat/rbx/main/windgate.lua'))()")
+	end
+end)
 
-task.wait(5)
 -- END OF AUTO-RERUN --
 
 -- WAIT UNTIL WINDGATE PLAYER IS READY --
